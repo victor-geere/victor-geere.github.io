5d30c333b2c81840520cafd87e02ebf38f3b9989
Victor Geere <victorgeere@gmail.com>
2020-09-25 23:38:00 +0200

    fix colors on orbits

--- vector-calculus/colors.js
+++ vector-calculus/colors.js
@@ -38,28 +38,28 @@ const colorsB = [
     '#ffb0da',
     '#ffb0ea',
     '#ffb0fa',
-    
+
     '#efc0aa',
     '#efc0ba',
     '#efc0ca',
     '#efc0da',
     '#efc0ea',
     '#efc0fa',
-    
+
     '#dfd0aa',
     '#dfd0ba',
     '#dfd0ca',
     '#dfd0da',
     '#dfd0ea',
     '#dfd0fa',
-    
+
     '#cfe0aa',
     '#cfe0ba',
     '#cfe0ca',
     '#cfe0da',
     '#cfe0ea',
     '#cfe0fa',
-    
+
     '#bff0aa',
     '#bff0ba',
     '#bff0ca',
@@ -78,4 +78,22 @@ const colorsGrey = [
     '#d0d0d0',
 ];
 
-export { colorsA, colorsB, colorsGrey }
+const rainbow = [
+    '#b4c8b4', '#b8c4b0', '#c0c4a8', '#c8c4a0',
+    '#d0c098', '#d8bc90', '#dcb88c', '#e4b484',
+    '#e8b080', '#eca87c', '#f0a478', '#f49c74',
+    '#f89870', '#f89070', '#f88870', '#fc846c',
+    '#f87c70', '#f87470', '#f86c70', '#f46874',
+    '#f06078', '#ec5c7c', '#e85480', '#e45084',
+    '#dc4c8c', '#d84890', '#d04498', '#c840a0',
+    '#c040a8', '#b840b0', '#b440b4', '#ac40bc',
+    '#a440c4', '#9c40cc', '#9444d4', '#8c48dc',
+    '#884ce0', '#8050e8', '#7c54ec', '#785cf0',
+    '#7460f4', '#7068f8', '#6c6cfc', '#6c74fc',
+    '#6c7cfc', '#6c80fc', '#6c88fc', '#6c90fc',
+    '#6c98fc', '#709cf8', '#74a4f4', '#78a8f0',
+    '#7cb0ec', '#80b4e8', '#88b8e0', '#8cbcdc',
+    '#94c0d4', '#9cc4cc', '#a4c4c4', '#acc4bc'
+];
+
+export { colorsA, colorsB, colorsGrey, rainbow }
