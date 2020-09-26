59b29ae0ea52cfa630accac7fdaea810ce46f778
Victor Geere <victorgeere@gmail.com>
2020-09-26 19:36:31 +0200

    add eta difference of terms

---
+++ eta-term-diffs/mathtools.js
@@ -0,0 +1,39 @@
+import {Vector3}  from './lib/three.module.js';
+
+function polarCos(degrees, radius, offset) {
+    return math.cos(math.pi * degrees / 180) * radius + offset;
+}
+
+function polarSin(degrees, radius, offset) {
+    return math.sin(math.pi * degrees / 180) * radius + offset;
+}
+
+function square(n) {
+    return n * n;
+}
+
+function getDistance(vector1, vector2) {
+    return Math.sqrt(square(vector1.x - vector2.x) + square(vector1.y - vector2.y));
+}
+
+function addVectors(vector1, vector2) {
+    return new Vector3(vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z);
+}
+
+function subVectors(vector1, vector2) {
+    return new Vector3(vector1.x - vector2.x, vector1.y - vector2.y, vector1.z - vector2.z);
+}
+
+function subVectorsXY(vector1, vector2) {
+    return new Vector3(vector1.x - vector2.x, vector1.y - vector2.y, vector1.z);
+}
+
+function moveVectors(vectorArray, x, y, z) {
+    vectorArray.forEach((vector) => {
+        vector.x = vector.x + x;
+        vector.y = vector.y + y;
+        vector.z = vector.z + z;
+    });
+}
+
+export { polarCos, polarSin, square, getDistance, addVectors, subVectors, moveVectors, subVectorsXY }
---
+++ eta-term-diffs/threetools.js
@@ -0,0 +1,160 @@
+import { polarCos, polarSin } from "./mathtools.js";
+import * as THREE from './lib/three.module.js';
+import {OrbitControls} from './lib/OrbitControls.js';
+
+function resize(renderer, camera, x, y) {
+    renderer.setSize(x, y);
+    camera.aspect = aspectRatio(x, y);
+    camera.updateProjectionMatrix();
+}
+
+let aspectRatio = function (width, height) {
+    return width / height;
+};
+
+
+function getMaterial(palette, colorN) {
+    return new THREE.LineBasicMaterial({
+        color: palette[colorN]
+    })
+}
+
+function makeCurve(pivot, a, b, r, ngon, rotation, material, curvePoints = [], resolution) {
+    const obj = {
+        properties: {
+            pivot: pivot,
+            origin: {x: a, y: b, z: 0},
+            radius: r,
+            ngon: ngon,
+            rotation: rotation,
+            mat: material
+        },
+        curvePoints: curvePoints,
+        curve: null,
+        geoPoints: null,
+        geo: null,
+        mesh: null
+    };
+    if (curvePoints.length > 1) {
+        if (!resolution) {
+            obj.geo = new THREE.BufferGeometry().setFromPoints(curvePoints);
+            obj.mesh = new THREE.Line(obj.geo, obj.properties.mat);
+        } else {
+            obj.curve = new THREE.CatmullRomCurve3(curvePoints);
+            obj.geoPoints = obj.curve.getPoints(resolution);
+            obj.geo = new THREE.BufferGeometry().setFromPoints(obj.geoPoints);
+            obj.mesh = new THREE.Line(obj.geo, obj.properties.mat);
+        }
+    }
+    return obj;
+}
+
+function makeCircle(pivot, a, b, r, ngon, rotation, material) {
+    let curvePoints = getCirclePoints(pivot, a, b, r, ngon, rotation);
+    return makeCurve(pivot, a, b, r, ngon, rotation, material, curvePoints);
+}
+
+function getCirclePoints(pivot, a, b, r, ngon = 8, rotated = 0) {
+    let k = rotated;
+    let plotPoints = [];
+
+    while (k <= 360 + rotated) {
+        plotPoints.push(new THREE.Vector3(polarCos(k, r, a), polarSin(k, r, b), 0));
+        k = k + 360 / ngon;
+    }
+    return plotPoints;
+}
+
+function newSceneInfo() {
+    return {
+        objects: [],
+        hasChanged: false,
+        animationLoopId: null,
+        renderer: null,
+        camera: null,
+        scene: null,
+        group: null,
+        scale: function (size) {
+            this.group.scale.set(size, size, size);
+        },
+        addObjectToScene: function (obj) {
+            this.scene.add(obj);
+        },
+        addObjectToGroup: function (obj3d) {
+            this.group.add(obj3d);
+        },
+        addObject: function (obj) {
+            this.objects.push(obj);
+            this.addObjectToGroup(obj.mesh);
+        },
+        clear: function () {
+            while (this.group.children.length > 0) {
+                this.group.remove(this.group.children[this.group.children.length - 1]);
+            }
+            this.objects = [];
+        },
+        render: function () {
+            this.renderer.render(this.scene, this.camera);
+        }
+    };
+}
+
+function buildScene(parentHtmlElement, x, y, scale, backgroudColor, lightColor) {
+    let info = newSceneInfo();
+    info.renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+    });
+    parentHtmlElement.appendChild(info.renderer.domElement);
+
+    info.camera = new THREE.PerspectiveCamera(32, aspectRatio(x, y), 1, 1000);
+    info.camera.position.set(0, 0, 50);
+
+    resize(info.renderer, info.camera, x, y);
+
+    info.scene = new THREE.Scene();
+    info.scene.background = new THREE.Color(backgroudColor);
+
+    const light = new THREE.DirectionalLight(lightColor, 1, Infinity);
+    light.position.set(0, 0, 0);
+    info.camera.add(light);
+    info.addObjectToScene(info.camera);
+
+    // controls
+    let controls = new OrbitControls(info.camera, info.renderer.domElement);
+    controls.minDistance = 10;
+    controls.maxDistance = 100;
+    controls.maxPolarAngle = Math.PI / 2;
+
+    // populate the scene
+    info.group = new THREE.Group();
+    info.scale(scale);
+    info.addObjectToScene(info.group);
+    return info;
+}
+
+function drawAxis(parent, size, material) {
+    let geoPoints = {
+        x: [
+            new THREE.Vector3(-size, 0, 0),
+            new THREE.Vector3( size, 0, 0),
+        ],
+        y: [
+            new THREE.Vector3(0, -size, 0),
+            new THREE.Vector3(0,  size, 0),
+        ],
+        z: [
+            new THREE.Vector3(0, 0, -size),
+            new THREE.Vector3(0, 0,  size),
+        ],
+    };
+    parent.add(new THREE.Line(new THREE.BufferGeometry().setFromPoints(geoPoints.x), material));
+    parent.add(new THREE.Line(new THREE.BufferGeometry().setFromPoints(geoPoints.y), material));
+    parent.add(new THREE.Line(new THREE.BufferGeometry().setFromPoints(geoPoints.z), material));
+}
+
+function newVec(x, y, z) {
+    return new THREE.Vector3(x, y, z);
+}
+
+export { getMaterial, drawAxis, getCirclePoints, makeCircle, makeCurve, buildScene, resize, newVec }
