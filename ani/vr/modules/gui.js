922de13bf1c7123e8741ebc8531029e9450fc8e9
Victor Geere <victorgeere@gmail.com>
2020-10-27 18:14:13 +0200

    add ani/vr

---
+++ ani/vr/modules/gui.js
@@ -0,0 +1,18 @@
+import { GUI } from '../lib/dat.gui.module.js'
+
+function popGui(gui) {
+    gui.controllers = {};
+    gui.parameters = {};
+    gui.params = {};
+    gui.init.forEach((item) => {
+        gui.parameters[item.param] = item.default;
+    });
+    gui.options.load = gui.parameters;
+    const datGui = new GUI(gui.options);
+    gui.init.forEach((item) => {
+        gui.controllers[item.ctrl] = datGui.add(gui.parameters, item.param, item.min, item.max, item.interval)
+            .onChange(item.onChange).onFinishChange(item.onFinishChange);
+    });
+}
+
+export { popGui }
