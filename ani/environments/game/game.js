4278814d0ad4719e1ea2a5cb2031ee0385fb678f
Victor Geere <victorgeere@gmail.com>
2020-12-29 14:07:11 +0200

    add environmnents with postprocessing bloom

---
+++ ani/environments/game/game.js
@@ -0,0 +1,74 @@
+import { getPlayer } from "./player.js";
+
+function makeGame(strategy) {
+    const game = {
+        strategy: null,
+        maxBalance: 0,
+        minBalance: 0,
+        n: 0,
+        ticks: 0,
+        risk: 0,
+        plot: [],
+        width: 600,
+        points: [{ x: 0, y:0 }, { x: 1, y:1 }],
+        players: []
+    };
+    game.totalBalance = function () {
+        let balance = 0;
+        this.players.forEach((player) => {
+            balance += player.balance;
+        });
+        return balance;
+    };
+    game.getStats = function() {
+        return this.players[0].getStats();
+    };
+    game.play = function () {
+        const result = Math.round(Math.random() * 10) % 2 === 1;
+        const turn = this.players[0].turn();
+        const win = turn.position === result;
+        this.players[0].play(win);
+        this.pushPlot(this.players[0].getBalance());
+    };
+    game.pushPlot = function (y = this.totalBalance()) {
+        this.points.push({ x: this.n, y: y });
+        if (this.points.length > this.width) {
+            this.points.splice(0, 1);
+        }
+        this.n++;
+        this.ticks++;
+    };
+    game.setOptions = function(options) {
+        // this.risk = options.risk;
+    };
+    game.resetGame = function() {
+
+    };
+    game.testGame = function() {
+        let win = 0;
+        let lose = 0;
+        for(let n = 0; n < 1000000; n++) {
+            if (Math.round(Math.random() * 10) % 2 === 1) {
+                win++;
+            } else {
+                lose++;
+            }
+        }
+        console.log(`win : ${win}`);
+        console.log(`lose : ${lose}`);
+    };
+    game.start = function(strategy) {
+        this.testGame();
+        this.strategy = strategy;
+        this.players = [ getPlayer(this.strategy) ];
+    };
+
+    game.start(strategy);
+
+    game.score = function() {
+        console.log(this.players[0].strategy.getLog());
+    };
+    return game;
+}
+
+export { makeGame }
---
+++ ani/environments/modules/materials.js
@@ -0,0 +1,108 @@
+import { rainbow } from "./colors.js";
+import * as THREE from "../lib/three/build/three.module.js";
+
+let materialCache = {};
+
+
+function getMaterialOptions() {
+    return {
+        color: 0x808080,
+        transparent: true,
+        opacity: 0.5
+    }
+}
+
+function getCustomMaterial(palette, colorN, options) {
+    return new THREE.MeshBasicMaterial({...options, color: palette[colorN]});
+}
+
+function getMaterial(palette, colorN, opacity = 0) {
+    if (opacity) {
+        return new THREE.MeshBasicMaterial({
+            color: palette[colorN],
+            opacity: opacity,
+            transparent: true
+        })
+    }
+    return new THREE.MeshBasicMaterial({
+        color: palette[colorN]
+    })
+}
+
+function getPhongLight(colorHex, emissiveColorHex, intensity) {
+    return new THREE.MeshPhongMaterial({
+        color: colorHex,
+        opacity: 0.2,
+        transparent: true,
+        emissive: emissiveColorHex,
+        emissiveIntensity: intensity
+    })
+}
+
+function getPhongMaterial(palette, colorN, opacity = 0) {
+    if (opacity) {
+        return new THREE.MeshPhongMaterial({
+            color: palette[colorN],
+            opacity: opacity,
+            transparent: true
+        })
+    }
+    return new THREE.MeshPhongMaterial({
+        color: palette[colorN]
+    })
+}
+
+function getLambertMaterial(palette, colorN, opacity = 0) {
+    if (opacity) {
+        return new THREE.MeshLambertMaterial({
+            color: palette[colorN],
+            opacity: opacity,
+            transparent: true
+        })
+    }
+    return new THREE.MeshLambertMaterial({
+        color: palette[colorN]
+    })
+}
+
+function getLineMaterial(palette, colorN, opacity = 0) {
+    if (opacity) {
+        return new THREE.LineBasicMaterial({
+            color: palette[colorN],
+            opacity: opacity,
+            transparent: true
+        })
+    }
+    return new THREE.LineBasicMaterial({
+        color: palette[colorN]
+    })
+}
+
+function getOutlineMaterial() {
+    return new THREE.LineBasicMaterial( {
+        color: 0x707080,
+        opacity: 0.75,
+        transparent: true
+    } );
+}
+
+function getPointsMaterial(colorN, size=0.125) {
+    let color = rainbow[colorN];
+    const key = `mat_${colorN}`;
+    if (materialCache[key]) {
+        return materialCache[key];
+    }
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('textures/sprites/disc.png');
+    const mat = new THREE.PointsMaterial({
+        color: color,
+        map: texture,
+        size: size,
+        alphaTest: 0.5
+    });
+    materialCache[key] = mat;
+    return mat;
+}
+
+export { getPointsMaterial, getMaterial, getLineMaterial, getCustomMaterial, getMaterialOptions,
+    getPhongMaterial, getOutlineMaterial, getPhongLight }
